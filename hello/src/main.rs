d509a49c110f59239067da71bc1fca778a69f426
gn-t-k <gntk1995422@gmail.com>
2022-08-27 09:05:32 +0900

    配列

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,11 +1,25 @@
-fn divided_by_5(num: u32) -> u32 {
-    if num == 0 {
-        return 0;
-    }
-    num / 5
-}
-
 fn main() {
-    let num = 25;
-    println!("{} divided by 5 = {}", num, divided_by_5(num))
+    // Days of the week
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    // Get the first day of the week
+    let first = days[0];
+
+    // Get the second day of the week
+    let second = days[1];
+
+    let never = days[8];
+
+    println!(
+        "days: {:?}, first: {}, second: {}, never: {:?}",
+        days, first, second, never
+    )
 }
