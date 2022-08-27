1e675a4ec7dd2f603ee568a6f6a2520894ee825f
gn-t-k <gntk1995422@gmail.com>
2022-08-27 19:59:30 +0900

    ベクター

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,25 +1,85 @@
+#[derive(PartialEq, Debug)]
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32),
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+#[derive(PartialEq, Debug)]
+enum Age {
+    New,
+    Used,
+}
+
+// Get the car quality by testing the value of the input argument
+// - miles (u32)
+// Create a tuple for the car quality with the Age ("New" or "Used") and mileage
+// Return a tuple with the arrow `->` syntax
+fn car_quality(miles: u32) -> (Age, u32) {
+    // Declare and initialize the return tuple value
+    // For a new car, set the miles to 0
+    (Age::New, miles)
+}
+
+// Build a new "Car" using the values of four input arguments
+// - color (String)
+// - motor (Transmission enum)
+// - roof (boolean, true if the car has a hard top roof)
+// - miles (u32)
+// Call the car_quality(miles) function to get the car age
+// Return an instance of a "Car" struct with the arrow `->` syntax
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    // Create a new "Car" instance as requested
+    // - Bind first three fields to values of input arguments
+    // - "age" field calls "car_quality" function with "miles" input argument
+    Car {
+        color,
+        motor,
+        roof,
+        age: car_quality(miles),
+    }
+}
+
 fn main() {
-    // Days of the week
-    let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ];
+    // Create car color array
+    let colors = ["Blue", "Green", "Red", "Silver"];
 
-    // Get the first day of the week
-    let first = days[0];
+    // Declare the car type and initial values
+    let mut car: Car = car_factory(String::from(colors[0]), Transmission::Manual, true, 0);
+    let mut engine: Transmission = Transmission::Manual;
 
-    // Get the second day of the week
-    let second = days[1];
+    // Order 3 cars, one car for each type of transmission
 
-    let never = days[8];
-
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(colors[0]), engine, true, 0);
     println!(
-        "days: {:?}, first: {}, second: {}, never: {:?}",
-        days, first, second, never
-    )
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), engine, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[2]), engine, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
